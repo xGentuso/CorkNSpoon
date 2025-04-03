40a4247098fdafa9642c5fd021ac6c0196c35d43
atoM <160615379+xGentuso@users.noreply.github.com>
2025-04-03 14:10:37 -0400

    Delete grocerly directory

--- grocerly/src/app/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id?: number;
-  email: string;
-  name: string;
-  // Add other user properties as needed
-}
--- grocerly/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  yummlyApiKey: "20a5449258msh75caf71cc4d39dap1ea7bajsn7b82d1d5e657",
-  yummlyApiUrl: "https://yummly2.p.rapidapi.com",
-  apiUrl: "http://localhost:3000/api"
-}; 
